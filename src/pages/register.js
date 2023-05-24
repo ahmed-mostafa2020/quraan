ab363bff49ea38269068589aed575e5eff594ea6
Ahmed Mostafa <ahmed-mostafa2020>
2023-05-24 23:28:42 +0300

    2

--- src/pages/register.js
+++ src/pages/register.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Image from "next/image";
 import image from '../../public/photo10.svg';
 import Link from 'next/link';
--- src/pages/result.js
+++ src/pages/result.js
@@ -1,6 +1,16 @@
+import Head from "next/head";
 
-export default function Result () {
+export default function Result() {
   return (
-    <div>result</div>
+    <>
+      <Head>
+        <title>جروب القرءان الكريم|النتيجة</title>
+        <meta name="description" content="جروب القراءن الكريم" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/blob14.svg" />
+      </Head>
+      <div>result</div>
+
+    </>
   )
 }
