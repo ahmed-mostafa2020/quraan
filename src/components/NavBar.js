478990610f11abb01bb2239fb826c69271b3a335
Ahmed Mostafa <ahmed-mostafa2020>
2023-05-31 23:05:39 +0300

    style1

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -16,10 +16,11 @@ export default function NavBar() {
 
           <h1 className="goods"> لِّيَدَّبَّرُوا  <span>آيَاتِهِ </span> </h1>
           <div>
-            <Link href='/' className="mr-2 p-1">الرئيسية </Link>
+            {/* <Link href='/' className="mr-2 p-1">الرئيسية </Link>
             <Link href='/login' className="mr-2 p-1">Login </Link>
             <Link href='/register' className="p-1">Register </Link>
-            <Link href='/result' className="p-1">result </Link>
+            <Link href='/result' className="p-1">result </Link> */}
+            <p>الموقع تحت التطوير </p>
           </div>
 
           <Link href='https://chat.whatsapp.com/Hokw6zJMbIF9Z43SUYOZo6' className="whats" target="blank"><IoLogoWhatsapp /></Link>
