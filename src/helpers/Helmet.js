f35043823d06e0330a8c2eb7306266cc7c87f2c8
Ahmed Mostafa <ahmed-mostafa2020>
2023-06-26 16:44:43 +0300

    still bad website

--- src/helpers/Helmet.js
+++ src/helpers/Helmet.js
@@ -1,17 +1,16 @@
-import Head from "next/head"
+import Head from "next/head";
 
-const Helmet = ({title}) => {
+const Helmet = ({ title }) => {
   return (
     <>
       <Head>
         <title>جروب القرءان الكريم | {title}</title>
         <meta name="description" content="جروب القراءن الكريم" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/blob14.svg" />
+        <link rel="icon" href="download (1).jpg" />
       </Head>
-
     </>
-  )
-}
+  );
+};
 
 export default Helmet;
